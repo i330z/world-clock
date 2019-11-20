747e8c28df98fe1ff3c9880f1f43549ac709f727
i330z <izzazahmed134@gmail.com>
2019-11-20 17:02:10 +0530

    init commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/service/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin',flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime' : instance.isDayTime,
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    setupWorldTime();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
